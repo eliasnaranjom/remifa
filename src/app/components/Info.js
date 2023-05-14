cc485d8754bdb5832f87c701f8dc9f8c9e8f4d8e
Elias Naranjo <eliasnaranjom@gmail.com>
2023-05-14 01:44:38 -0500

    Correccion de error Key

--- src/app/components/Info.js
+++ src/app/components/Info.js
@@ -1,15 +1,15 @@
 import '../css/Info.css';
 
-const Info = props => {
+const Info = ({ClassNameDirection, Img, Title, Description}) => {
 
     return (
-        <section className={props.classNameDirection} id='Info'>
+        <section className={ClassNameDirection} id='Info'>
             <div className='InfoImg'>
-                <img src={`../${props.Img}`} alt='Guitar' />
+                <img src={`../${Img}`} alt='Guitar' />
             </div>
             <div className='InfoText'>
-                <h1>{props.Title}</h1>
-                <p>{props.Description}</p>
+                <h1>{Title}</h1>
+                <p>{Description}</p>
             </div>
         </section>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -30,9 +30,9 @@ export default function Home() {
 
         <CoursesCarousel data={data} />
 
-        <Info Img='guitar.jpg' classNameDirection='InfoRight' Title='Formando músicos integrales' Description='Creemos que la música es un arte que trasciende fronteras y enriquece el alma humana, por lo que nuestra fundación está dedicada a ofrecer una educación musical de excelencia que fomente la creatividad, la pasión y el compromiso con la sociedad, para formar músicos integrales que inspiren y transformen el mundo con su arte.' />
+        <Info Img='guitar.jpg' ClassNameDirection='InfoRight' Title='Formando músicos integrales' Description='Creemos que la música es un arte que trasciende fronteras y enriquece el alma humana, por lo que nuestra fundación está dedicada a ofrecer una educación musical de excelencia que fomente la creatividad, la pasión y el compromiso con la sociedad, para formar músicos integrales que inspiren y transformen el mundo con su arte.' />
 
-        <Info Img='mic.jpg' classNameDirection='InfoLeft' Title='Música para transformar vidas' Description='En nuestra fundación creemos que la música es un medio para potenciar el desarrollo humano, por lo que nos enfocamos en brindar una formación musical que no solo fomente habilidades técnicas, sino que también cultive valores como la disciplina, la resiliencia y el trabajo en equipo, para formar músicos comprometidos en transformar vidas a través del poder del arte.' />
+        <Info Img='mic.jpg' ClassNameDirection='InfoLeft' Title='Música para transformar vidas' Description='En nuestra fundación creemos que la música es un medio para potenciar el desarrollo humano, por lo que nos enfocamos en brindar una formación musical que no solo fomente habilidades técnicas, sino que también cultive valores como la disciplina, la resiliencia y el trabajo en equipo, para formar músicos comprometidos en transformar vidas a través del poder del arte.' />
 
         {/* <section>
           Equipo de trabajo
